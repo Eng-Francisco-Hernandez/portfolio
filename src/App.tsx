f074c8fbfbeb3759ad9302a4db4d14b771c9e14e
Eng-Francisco-Hernandez <fjch87@gmail.com>
2023-01-31 01:47:45 -0600

    feat(landing-page): Added landing page initial framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Landing } from './pages';
+
+const customTheme = createTheme({
+  palette: {
+    // mode: "dark",
+    primary: {
+      main: "#011c7e",
+      light: "#4e73fe",
+      dark: "#01114c",
+    },
+    text: {
+      primary: "#01114c",
+      secondary: "#0127b1",
+      disabled: "#819bfe",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <ThemeProvider theme={customTheme}>
+      <div className="App">
+        <Landing></Landing>
+      </div>
+    </ThemeProvider>
   );
 }
 
